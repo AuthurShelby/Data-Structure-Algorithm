47525311c4d1ca0c836d045007597c196ba3ac40
Devanand Puzhakkool <132560246+AuthurShelby@users.noreply.github.com>
2025-03-05 23:33:01 +0530

    Made changes like asking for number of disks
    Added a few comments

--- TOH.py
+++ TOH.py
@@ -1,9 +1,11 @@
-def toh(n , source , auxi , dest):
+def tower_of_hanoi(n, source, auxiliary, destination):
     if n == 1:
-        print(f'Disk {n} moved from {source} to {dest}')
-        return 1
-    toh(n-1 , source , dest , auxi)
-    print(f'Disk {n} moved from {source} to {dest}')
-    toh(n-1 , auxi , source , dest)
-if __name__ == '__main__':
-    toh(5, 1 , 2 , 3)
+        print(f"Move disk {n} from rod {source} to rod {destination}")
+        return
+    tower_of_hanoi(n - 1, source, destination, auxiliary)  # Move n-1 disks to auxiliary peg
+    print(f"Move disk {n} from rod {source} to rod {destination}")  # Move nth disk to destination
+    tower_of_hanoi(n - 1, auxiliary, source, destination)  # Move n-1 disks from auxiliary to destination
+
+# Example usage
+n = int(input('Enter the number of : '))  # Change this value for different number of disks
+tower_of_hanoi(n, 'A', 'B', 'C')
