53a73067d22174dec083ad3db3487bdb01e82c24
Devanand Puzhakkool <132560246+AuthurShelby@users.noreply.github.com>
2025-04-04 22:53:12 +0530

    Add files via upload

--- Sorting Algorithms/BucketSort.py
+++ Sorting Algorithms/BucketSort.py
@@ -36,4 +36,7 @@ def bucketSort(arr):
     
     return sorted_array
 
-print(bucketSort([0.78, 0.17, 0.39, 0.26, 0.72, 0.94, 0.21, 0.12, 0.23, 0.68]))
+# Execution
+if __name__ == '__main__':
+    arr = list(map(int , input('Enter elements with space -> ').split()))
+    print(bucketSort(arr))
