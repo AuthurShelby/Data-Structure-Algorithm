585c9465c1da45c8fbbd39693539510f8e9c223d
Devanand Puzhakkool <132560246+AuthurShelby@users.noreply.github.com>
2025-04-07 16:46:13 +0530

    creating board and updating the mines

--- Puzzle/Minesweeper.py
+++ Puzzle/Minesweeper.py
@@ -21,4 +21,36 @@ else:
   mine_density = 0.15
 
 # calculate the mines
-MINE = int(ROWS*COLS*mine_density)
+MINES = int(ROWS*COLS*mine_density)
+
+# printing info
+print(f'Board Size - {SIZE}')
+print(f'Game Difficulty - {difficulty}')
+print(f'Mines set - {MINES}')
+
+# creating the board
+def create_board():
+  
+  # Initializing the board 
+  board = [[' ' for i in range(COLS)] for i in range(ROWS)]
+  # mines to store the position
+  mines = set()
+  
+  # randomly placing the mines until the number of position is equal to MINES
+  while len(mines) < MINES:
+
+    row_index , col_index = random.randint(0,ROWS-1) , random.randint(0 , COLS-1)
+
+    # if the (row,col) is not in the mines position
+    # also this removes the entry of duplicate position
+    if (row_index , col_index) not in mines: 
+
+      mines.add((row_index , col_index))
+
+      # making changes in the board
+      # setting 'M' for mine
+      board[row_index][col_index] = 'M'  
+
+  return board
+
+
